aa0cd6cf385e462534ac0be92c09e954c043257e
NikolaTesla13 <asandei.stefanel@gmail.com>
2020-05-30 14:13:40 +0300

    Updates

---
+++ precache-manifest.22850a0ddca794578e889913e940dd9e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5345d54edf9479d485e07b059957ae4a",
+    "url": "/restaurant-website/index.html"
+  },
+  {
+    "revision": "0e577db075f593f5d520",
+    "url": "/restaurant-website/static/css/main.61602328.chunk.css"
+  },
+  {
+    "revision": "10ce3b03d1d9f20ad821",
+    "url": "/restaurant-website/static/js/2.e7b55b8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/restaurant-website/static/js/2.e7b55b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e577db075f593f5d520",
+    "url": "/restaurant-website/static/js/main.d4d5029d.chunk.js"
+  },
+  {
+    "revision": "56d22c1dc4e4598771f9",
+    "url": "/restaurant-website/static/js/runtime-main.98f704bc.js"
+  },
+  {
+    "revision": "c6f8e847657e3c51ea6479742efbcdcc",
+    "url": "/restaurant-website/static/media/box2.c6f8e847.jpg"
+  },
+  {
+    "revision": "5973852fc29ca24ae55a7e5e0e176a96",
+    "url": "/restaurant-website/static/media/box3.5973852f.jpg"
+  },
+  {
+    "revision": "0e7e766cf67bc1767dc15cff0599cb9e",
+    "url": "/restaurant-website/static/media/land.0e7e766c.jpg"
+  },
+  {
+    "revision": "b5dbe610b9a4fbacb4f90388651dcf93",
+    "url": "/restaurant-website/static/media/pizza.b5dbe610.jpg"
+  }
+]);
