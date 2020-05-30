af366ce05139cd4042da3f717512701370f4c686
NikolaTesla13 <asandei.stefanel@gmail.com>
2020-05-30 14:16:00 +0300

    Your awesome message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,159 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div id="app">
+      <header>
+        <nav>   
+          <ul>
+            <li class="title"><h1 class="title">Restaurant</h1></li>
+            <li class="notitle"><a href="#">About</a></li>
+            <li class="notitle"><a href="#">Servings</a></li>
+            <li class="notitle"><a href="#">Rezervations</a></li>
+            <li class="notitle"><a href="#">Photos</a></li>
+          </ul>
+        </nav>
       </header>
+      <div className="landing">
+        <div className="box">
+          <h1>Restaurant</h1>
+        </div>
+        <br></br>
+      </div>
+      <div className="main1">
+        <div className="box1">
+            <h1>Rezervations</h1> 
+            <br></br>
+            <div className="box-text">
+              <h3>A table reservation is an arrangement made in<br></br> advance to have a table<br></br> available at a restaurant. While mo<br></br>st restaurants in the vast majority of the world do not <br></br>require a reservation, and some do not have<br></br> a policy or simply any channel for making<br></br> one, so-called higher-end restaurants mainly in on.</h3>
+            </div>
+          <br></br>
+          <button className="btn">
+            book a table 
+          </button>
+        </div>
+        <div className="box2">
+        </div>
+        <div className="box3">
+        </div>
+        <div className="box1">
+            <h1>Rezervations</h1> 
+            <br></br>
+            <div className="box-text">
+            <h3>A table reservation is an arrangement made in<br></br> advance to have a table<br></br> available at a restaurant. While mo<br></br>st restaurants in the vast majority of the world do not <br></br>require a reservation, and some do not have<br></br> a policy or simply any channel for making<br></br> one, so-called higher-end restaurants mainly in on.</h3>
+            </div>
+          <br></br>
+          <button className="btn">
+            book a table 
+          </button>
+        </div>
+      </div>
+      <hr></hr>
+      <br></br><br></br><br></br><br></br><br></br>
+      <div className="separt">
+        <center>
+          <h1>F O O D</h1>
+        </center>
+      </div>
+      <br></br><br></br><br></br><br></br><br></br>
+      <hr></hr>
+      <div className="mini-gallery">
+        <div className="gal">
+          <div className="gal01">
+            <h1>Pizza</h1>
+            <p>Pizza is a savory dish of Italian origin, consisting of a usually round, flattened base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients baked at a high temperature, traditionally in a wood-fired oven. A small pizza is sometimes called a pizzetta. </p>
+          </div>
+          <div className="gal02"></div>
+        </div>
+        <div className="gal1">
+          <div className="gal11">            
+          <h1>Pizza</h1>
+            <p>Pizza is a savory dish of Italian origin, consisting of a usually round, flattened base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients baked at a high temperature, traditionally in a wood-fired oven. A small pizza is sometimes called a pizzetta. </p>
+          </div>
+          <div className="gal12"></div>
+        </div>
+        <div className="gal2">
+          <div className="gal21">
+          </div>
+          <div className="gal22">
+          <h1>Pizza</h1>
+            <p>Pizza is a savory dish of Italian origin, consisting of a usually round, flattened base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients baked at a high temperature, traditionally in a wood-fired oven. A small pizza is sometimes called a pizzetta. </p>
+          </div>
+        </div>
+        <div className="gal3">
+          <div className="gal31">
+          <h1>Pizza</h1>
+            <p>Pizza is a savory dish of Italian origin, consisting of a usually round, flattened base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients baked at a high temperature, traditionally in a wood-fired oven. A small pizza is sometimes called a pizzetta. </p>
+          
+          </div>
+          <div className="gal32"></div>
+        </div>
+        <div className="gal4">
+          <div className="gal41">
+          <h1>Pizza</h1>
+            <p>Pizza is a savory dish of Italian origin, consisting of a usually round, flattened base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients baked at a high temperature, traditionally in a wood-fired oven. A small pizza is sometimes called a pizzetta. </p>
+          
+          </div>
+          <div className="gal42"></div>
+        </div>
+        <div className="gal5">
+          <div className="gal51">
+          <h1>Pizza</h1>
+            <p>Pizza is a savory dish of Italian origin, consisting of a usually round, flattened base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients baked at a high temperature, traditionally in a wood-fired oven. A small pizza is sometimes called a pizzetta. </p>
+          
+          </div>
+          <div className="gal52"></div>
+        </div>
+      </div>
+      <br></br><br></br><br></br><br></br><br></br>
+      <div className="separt">
+        <center>
+          <h1></h1>
+        </center>
+      </div>
+      <br></br><br></br><br></br><br></br><br></br>
+      <div className="footer"> 		
+        <div className="inner-footer"> 			
+          <div className="footer-items"> 				
+            <h1>Restaurant</h1> 				
+            <p> 					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod 					tempor incididunt ut labore et dolore magna aliqua. 				</p>
+          </div> 	
+          <div className="footer-items"> 				
+            <h2>Quick Links</h2> 				
+            <div className="border"></div> 				
+            <ul>
+    					<a href=""><li>Home</li></a> 					
+              <a href=""><li>About</li></a> 	
+              <a href=""><li>Servings</li></a> 					
+              <a href=""><li>Rezervations</li></a> 	
+              <a href=""><li>Photos</li></a> 	
+            </ul> 			
+          </div> 			
+        <div className="footer-items"> 				
+          <h2>specialities</h2> 				
+          <div className="border"></div> 				
+          <ul> 					
+            <a href=""><li>Soup</li></a> 			
+            <a href=""><li>Fries</li></a>	
+            <a href=""><li>Pizza</li></a> 				
+            <a href=""><li>Salad</li></a> 
+            <a href=""><li>Rice</li></a> 				
+          </ul> 			
+        </div> 			
+        <div className="footer-items"> 				
+          <h2>Contact us</h2> 				
+          <div className="border"></div> 				
+          <ul> 					
+            <a href=""><li>Iasi, Iasi, Romania</li></a> 			
+            <a href=""><li>+44 564 456 987</li></a> 			
+          </ul> 			
+        </div> 					 	
+    	</div> 		
+      <div className="footer-bottom"> 			
+        Copyright &copy; Computers & Codes 2019. All rights reserved. 		
+      </div> 	
+    </div>
     </div>
   );
 }
